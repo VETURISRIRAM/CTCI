fd83db16062a907e0e3995879e8b08abfaf62e50
SRIRAM VETURI <sriram.tutu@gmail.com>
2018-04-15 15:09:19 -0500

    Update 3.py

--- Chapter 1/3.py
+++ Chapter 1/3.py
@@ -1,15 +1,9 @@
-def duplicateRemoval(string):
-	
+def duplicateRemoval(string):	
 	for x in range(0, len(string)):
 		for y in range(x+1, len(string)):
 			if string[x] == string[y]:
 				string = string.replace(string[y],"")
-
 	return string
-
-
-
-
 
 if __name__ == "__main__":
 	s = "sriram"
